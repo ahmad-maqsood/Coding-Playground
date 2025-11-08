5ba35ddbcb1c58bb56ea632e7110a6d3692349fb
Ahmad Maqsood <am9566222@gmail.com>
2025-11-08 07:46:48 +0500

    Add files via upload

---
+++ Counter Program(HTML-CSS-JS)/script.js
@@ -0,0 +1,21 @@
+let increase = document.getElementById("incButton");
+let decrease = document.getElementById("decButton");
+let reset = document.getElementById("resetButton");
+let counter = document.getElementById("counter");
+
+let count = 0;   //JS var
+
+increase.onclick = function(){
+count++;
+counter.textContent = count++;
+}
+
+decrease.onclick = function(){
+count--;
+counter.textContent = count;
+}
+
+reset.onclick = function(){
+count = 0;
+counter.textContent = count;
+}
